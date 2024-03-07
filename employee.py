18a41721cb7a48640362b40352edec4b004a1a43
aziznosirov <67833332+aziznosirov@users.noreply.github.com>
2024-03-07 18:34:37 -0500

    Update employee.py

--- employee.py
+++ employee.py
@@ -7,6 +7,7 @@ This module contains classes for managing employees, projects, and tasks in a fi
 class Employee:
     """
     Class representing an employee in the company.
+    
 
     Attributes:
         name (str): The name of the employee.
@@ -14,15 +15,24 @@ class Employee:
         role (str): The role of the employee.
         salary (float): The salary of the employee.
     """
+    name : str
+    emp_id : str
+    role : str
+    salary : float
 
     def __init__(self, name, emp_id, role, salary):
         """
         Initialize an Employee object.
-
+        
         Args:
             name (str): The name of the employee.
             emp_id (str): The employee ID.
             role (str): The role of the employee.
             salary (float): The salary of the employee.
         """
-        pass
+
+        self.name = name
+        self.emp_id = emp_id
+        self.role = role
+        self.salary = salary
+        
